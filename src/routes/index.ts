c1efac2b61b95131fb8b62bb573f8c6426de6a19
Reimon Ferreira <reimonf@gmail.com>
2020-06-03 17:51:57 -0300

    Config Debug

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,8 +2,13 @@ import { Router } from 'express';
 
 const routes = Router();
 
-routes.get('/', (request, response) =>
-  response.json({ message: 'System base' })
-);
+routes.post('/users', (request, response) => {
+  const { name, email } = request.body;
+  const user = {
+    name,
+    email,
+  };
+  return response.json(user);
+});
 
 export default routes;
