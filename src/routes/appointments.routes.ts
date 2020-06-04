1d94e4edcba9b6a5f24e2c5723fd298bde4f9e23
Reimon Ferreira <reimonf@gmail.com>
2020-06-04 07:41:14 -0300

    add date-fns

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,5 +1,25 @@
 import { Router } from 'express';
+import { uuid } from 'uuidv4';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
 
 const appointmentsRouter = Router();
+
+const appointments = [];
+
+appointmentsRouter.post('/', (request, response) => {
+  const { provider, date } = request.body;
+
+  const parseDate = startOfHour(parseISO(date));
+  const findAppointmentInSameDate =
+
+  const appointment = {
+    id: uuid(),
+    provider,
+    parseDate,
+  };
+  appointments.push(appointment);
+
+  return response.json(appointment);
+});
 
 export default appointmentsRouter;
